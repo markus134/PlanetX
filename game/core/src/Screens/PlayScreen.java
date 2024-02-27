e8a4040c671574d74f0515c86f5137940e95ce25
Markus Veerbaum <mveerb@taltech.ee>
2024-02-27 13:11:12 +0200

    refactor code

--- game/core/src/Screens/PlayScreen.java
+++ game/core/src/Screens/PlayScreen.java
@@ -183,7 +183,7 @@ public class PlayScreen implements Screen {
         renderer.render();
 
         // Uncomment the following line if you want to see box2d lines
-        b2dr.render(world, gameCam.combined);
+        // b2dr.render(world, gameCam.combined);
 
         game.batch.setProjectionMatrix(gameCam.combined);
         game.batch.begin();
